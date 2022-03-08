05018a781122e048a51733643bdfbf92c75a1453
annyhira414 <annyhira@gmail.com>
2022-03-08 16:44:07 +0600

    using onChange mathod and usestate hook

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
+import React,{useState} from "react";
 import "./App.css";
 
 function App() {
+  const [name ,setName] = useState("");
+  const [email,setEmail] = useState("");
+  const [address , setAddress] = useState("");
   return (
     <div className="App">
       <h1>my user app.js</h1>
       <div style={{ display: "inline-block" }}> 
         <div>
           <label>Name </label>
-          <input type="text"></input>
+          <input type="text"
+          onChange={(e)=>setName(e.target.value)} value={name}></input>
+       
           <br />
           <label>Email </label>
-          <input type="email"></input>
+          <input type="email" onChange={(e) => setEmail(e.target.value)} value ={email}></input>
           <br />
           <label>Address </label>
-          <input type="text"></input>
+          <input type="text" onChange={(e) => setAddress(e.target.value)} value={address}></input>
         </div>
       </div>
     </div>
