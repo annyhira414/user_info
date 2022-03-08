6af9921830f321d273d148802fdce5c825e7d59b
annyhira414 <annyhira@gmail.com>
2022-03-08 12:49:07 +0600

    input box create in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-    
+      <h1>my user app.js</h1>
+      <div style={{ display: "inline-block" }}> 
+        <div>
+          <label>Name </label>
+          <input type="text"></input>
+          <br />
+          <label>Email </label>
+          <input type="email"></input>
+          <br />
+          <label>Address </label>
+          <input type="text"></input>
+        </div>
+      </div>
     </div>
   );
 }
